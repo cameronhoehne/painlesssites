a4b2412aeefe93e644992d88c5e28319690f376c
cameronhoehne <98066436+cameronhoehne@users.noreply.github.com>
2026-05-01 18:17:50 -0500

    Updated code to use njk variables

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,21 +1,21 @@
 module.exports = {
-    "name": "Painless Sites",
-    "email": "contact@painlesssites.com",
-    "phoneForTel": "213-347-4319",
-    "phoneFormatted": "(213) 347-4319",
-    "address": {
-        "lineOne": "",
-        "lineTwo": "",
-        "city": "Paducah",
-        "state": "KY",
-        "zip": "42001",
-        "country": "US",
-        "mapLink": ""
-    },
-    "socials": {
-        "facebook": "",
-        "instagram": "",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://painlesssites.com"
+  name: "Painless Sites",
+  email: "contact@painlesssites.com",
+  phoneForTel: "213.347.4319",
+  phoneFormatted: "(213) 347-4319",
+  address: {
+    lineOne: "",
+    lineTwo: "",
+    city: "Paducah",
+    state: "KY",
+    zip: "42001",
+    country: "US",
+    mapLink: "",
+  },
+  socials: {
+    facebook: "",
+    instagram: "",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  domain: "https://painlesssites.com",
 };
